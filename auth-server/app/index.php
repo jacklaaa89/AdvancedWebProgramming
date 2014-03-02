8b70b2d49a2d11ef68db3f65312919cb8e6246f6
Jack <jacktimblin@gmail.com>
2014-03-02 15:25:59 +0000

    Nearly Completed auth server, just need to complete auth process of a user logging in.

--- auth-server/app/index.php
+++ auth-server/app/index.php
@@ -2,13 +2,14 @@
 
 use \Phalcon\Db\Adapter\Pdo\Mysql;
 
-//use loader class to auto-load required classes.
+//use loader class to not only auto-load required classes, but also
+//register the namespaces.
 $loader = new \Phalcon\Loader();
 
-$loader->registerDirs(
-	array(
-		'models/'
-	)
+$loader->registerNamespaces(
+    array(
+        "Models" => "../app/models/"
+    )
 )->register();
 
 //create a dependency injector, which is used to inject modules,
@@ -18,14 +19,21 @@ $di = new \Phalcon\DI\FactoryDefault();
 
 //set up a database connection service for the models to use.
 $di->set('db', function() {
-	return new Mysql(
-		array(
-			'host' => 'locahost',
-			'username' => 'root',
-			'password' => 'root',
-			'dbname' => 'oauth'
-		)
-	);
+    return new Mysql(
+        array(
+            'host' => 'locahost',
+            'username' => 'root',
+            'password' => 'root',
+            'dbname' => 'oauth'
+        )
+    );
+});
+
+//need to set a global key in the crypt for cookie management encryption.
+$di->set('crypt', function() {
+    $crypt = new Phalcon\Crypt();
+    $crypt->setKey('89K%1m1K1cK38z^NxIG9SZ}L61!M)S');
+    return $crypt;
 });
 
 //create a new new micro application
@@ -33,99 +41,142 @@ $app = new \Phalcon\Mvc\Micro($di);
 
 //define the routes used in this application. Each route is handled here rather than
 //using controllers, as all this dummy server provides is authorisation.
-
 $app->post('/oauth/token', function() use ($app) {
 
-	//get the json request body, i.e the client secret, id,
-	//timstamp and authorization code to request an access token.
-	$params = (array) $app->request->getJsonRawBody();
+    //get the json request body, i.e the client secret, id,
+    //timstamp and authorization code to request an access token.
+    $params = (array) $app->request->getJsonRawBody();
 
-	$response = new \Phalcon\Http\Response();
+    $response = new \Phalcon\Http\Response();
 
-	//format params.
-	$data = array();
-	//make all the keys in params lower.
-	foreach($params as $key => $value) {
-		$data[strtolower($key)] = $value;
-	}
+    //format params.
+    $data = array();
+    //make all the keys in params lower.
+    foreach ($params as $key => $value) {
+        $data[strtolower($key)] = $value;
+    }
 
-	//check that all of the required params have been set.
-	$keys = array(
-		'clientid',
-		'clientsecret',
-		'timestamp',
-		'signature'
-	);
+    //check that all of the required params have been set.
+    $keys = array(
+        'clientid',
+        'clientsecret',
+        'timestamp',
+        'signature',
+        'scope'
+    );
 
-	//by default the request is treated as valid
-	//until proven wrong.
-	$validRequest = true;
+    //by default the request is treated as valid
+    //until proven wrong.
+    $validRequest = true;
 
-	//check that the same keys are supplied, regardless of case and whatever
-	//params were provided by the user.
-	if(count(array_intersect($keys,array_keys($data))) != 4) {
-		$validRequest = false;
-	}
+    //check that the same keys are supplied, regardless of case and whatever
+    //params were provided by the user.
+    if (count(array_intersect($keys, array_keys($data))) != 4) {
+        $validRequest = false;
+    }
 
-	//check that the variables are of the required type, i.e
-	//the clientID/secret/signature are strings and timestamp can be
-	//parsed as a valid datetime.
+    //check that the variables are of the required type, i.e
+    //the clientID/secret/signature are strings and timestamp can be
+    //parsed as a valid datetime.
+    if (!date('Y-m-d', $data['timestamp'])) {
+        $validRequest = false;
+    }
+    
+    //we need to check that permissions were set.
+    if(!is_array($data['scope']) || count($data['scope']) == 0) {
+        $validRequest = false;
+    }
 
-	if(!date('Y-m-d', $data['timestamp'])) {
-		$validRequest = false;
-	}
+    foreach (array($data['clientid'], $data['clientsecret'], $data['signature']) as $value) {
+        if (!isset($value) && !is_string($value)) {
+            $validRequest = false;
+        }
+    }
 
-	foreach(array($data['clientid'], $data['clientsecret'], $data['signature']) as $value) {
-		if(!isset($value) && !is_string($value)) {
-			$validRequest = false;
-		}
-	}
+    //if any of the request tests failed, then send 400 - Bad Request.
+    if (!$validRequest) {
+        //set the status code to 400 - Bad Request
+        $response->setStatusCode(400, "Bad Request")->send();
+    }
 
-	//if any of the request tests failed, then send 400 - Bad Request.
-	if(!$validRequest) {
+    //after request is correctly parsed, try to determine if
+    //client exists.
 
-		//set the status code to 400 - Bad Request
-		$response->setStatusCode(400, "Bad Request");
+    $redirectURI = (isset($data['redirecturi'])) ? $data['redirecturi'] : null;
 
-		//send the response.
-		$response->send();
-	}
+    if (!\Models\Client::checkIsValidClient($data['clientid'], $data['clientsecret'])) {
+        //set the status code to 403 - Forbidden
+        $response->setStatusCode(403, "Forbidden")->send();
+    }
 
-	//after request is correctly parsed, try to determine if
-	//client exists.
+    $request = \Models\Request::requestBuilder($data['clientid'], $data['scope'], $data['timestamp'], $redirectURI);
+    if (!$request) {
+        //set the status code to 403 - Forbidden
+        $response->setStatusCode(403, "Forbidden")->send();
+    }
 
-	$redirectURI = (isset($data['redirecturi'])) ? $data['redirecturi'] : null;
-
-	if(!\Models\Client::checkIsValidClient($data['clientid'], $data['clientsecret'])) {
-
-		//set the status code to 403 - Forbidden
-		$response->setStatusCode(403, "Forbidden");
-
-		//send the response.
-		$response->send();
-	}
-
-	$request = \Models\Request::requestBuilder($data['clientid'], $data['timestamp'], $redirectURI);
-	if(!$request) {
-
-		//set the status code to 403 - Forbidden
-		$response->setStatusCode(403, "Forbidden");
-
-		//send the response.
-		$response->send();
-	}
-	
-
-	//start authorization process, create new request instance and send
-	//to /oauth/authorisation.
-
-	$response->redirect('/oauth/authorisation/'.$request->getRequestID());
-
-
+    //start authorization process, create new request instance and send
+    //to /oauth/authorisation.
+    $response->redirect('/oauth/authorisation/' . $request->getRequestID());
 });
 
 $app->get('/oauth/authorisation/{ri:[0-9A-Za-z]+}', function($ri) use ($app) {
-	die(var_dump(\Models\Request::findRequestByID($ri)));
+    
+    //check that the Request is valid.
+    $request = \Models\Request::findRequestByID($ri);
+    
+    //if the request is not found.
+    if(!$request) {
+        $app->response->setStatusCode(404, "Not Found")->send();
+    }
+    
+    $userID = null;
+    $previousPermissions = null;
+    //Check if a session is already set, i.e a user has authenticated before
+    //and asked to be remembered.
+    
+    //cookies on this auth site are not only encrypted but the only value that is
+    //stored is the userID which is only applicable for this site.
+    if($app->cookies->has('remember-me')) {
+        $userID = $app->cookies->get('remember-me')->getValue();
+    }
+    
+    //if a user has logged in before.
+    if(isset($userID)) {
+        //if the user has already authenticated, we need to check what
+        //the permissions required have not changed.
+        $previousPermissions = \Models\ClientPermissions::findPermissions($request->getClientID(), $userID);
+        
+        //check if the required permissions are the same.
+        $newPermissions = array();
+        foreach($request->getScope() as $permission) {
+            if(!$previousPermissions->hasPermission($permission)) {
+                $newPermissions[] = $permission;
+            }
+        }
+    
+        if(!empty($newPermissions)) {
+            //show permissions page with new permissions.
+        } else {
+            //just supply token as user has been authenticated
+            //before and all required permissions have been given permission.
+        }
+    }
+    
+    //show the log in page.
+    
+});
+
+$app->post('oauth/background/', function() use ($app) {
+    //this is used to perform ajax requests to move through the authenticated process.
+    //this is used to reload the HTML on the page without having to reload the page, with the
+    //only redirect being made is that to the redirectURI when the process is complete.
+});
+
+//Need to define a action which is fired if a route is used that is not found.
+//It just fires a 404 Not Found request.
+$app->notFound(function() use ($app) {
+    $app->response->setStatusCode(404, "Not Found")->send();
 });
 
 //handle the request.
--- auth-server/app/models/Request.php
+++ auth-server/app/models/Request.php
@@ -4,10 +4,9 @@ namespace Models;
 
 use Models\BaseModel,
     Phalcon\Db\Column,
-    Phalcon\Mvc\Model\Message,
     Phalcon\Mvc\Model\Validator\Uniqueness;
 
-/*
+/**
  * This class is an encapulation of an single request
  * that is made to this authorisation server. A request is saved 
  * in the database as a reference for when the authorisation URL requires
@@ -17,310 +16,321 @@ use Models\BaseModel,
  * @Author Jack Timblin - U1051575
  */
 class Request extends BaseModel {
+   
+    //the unique ID of this request.
+    private $requestID;
+    //the unique ID of the client 
+    //performing the request.
+    private $clientID;
+    //the redirect URI to use in this
+    //request as it may be provided.
+    private $redirectURI;
+    //the timestamp of when this request was generated.
+    private $timestamp;
+    //an array of permissions requested.
+    private $scope;
 
-	/**************/
-	/* Parameters */
+    /**
+     * This function overrides the getSource function in Model.
+     * It returns the database table that this Model is mapped to.
+     */
+    public function getSource() {
+        return "request";
+    }
 
-	//the unique ID of this request.
-	private $requestID;
+    public function initialize() {
+        $this->belongsTo('clientID', 'Client', 'clientID');
+    }
+    
+    /**
+     * Called by the model just before the database operation is executed.
+     * Glues the permissions array as a string for db storage.
+     */
+    public function beforeSave() {
+        $this->scope = join(',', $this->scope);
+    }
+    
+    /**
+     * Called just after the create operation is executed.
+     * explodes the db string to permissions array.
+     */
+    public function afterFetch() {
+        $this->scope = explode(",", $this->scope);
+    }
 
-	//the unique ID of the client 
-	//performing the request.
-	private $clientID;
+    /**
+     * This method is called before the model is saved to the database
+     * i.e when model->save() is called. This validates that the 
+     * requestID generated for this request is unique, and fails if not.
+     */
+    public function validation() {
+        //perform validation on this model before saving/updating.
+        //the requestID must be unique
+        $this->validate(new Uniqueness(
+                array(
+            'field' => 'requestID',
+            'message' => 'The requestID must be unique.'
+                )
+        ));
 
-	//the redirect URI to use in this
-	//request as it may be provided.
-	private $redirectURI;
+        //check if a validation message has been produced.
+        if ($this->validationHasFailed()) {
+            return false;
+        }
+    }
 
-	//the timestamp of when this request was generated.
-	private $timestamp;
-	
-	/* End Parameters */
-	/******************/
+    /**
+     * This function validates that this request is valid.
+     * i.e that all of the parameters in this request are valid.
+     * 
+     * @return boolean <b>TRUE</b> if request is valid, <b>FALSE</b> otherwise.
+     */
+    public function isValidRequest() {
 
-	/*
-	 * This function overrides the getSource function in Model.
-	 * It returns the database table that this Model is mapped to.
-	 */
-	public function getSource() {
-		return "request";
-	}
+        //this request is treated as valid by default.
+        $valid = true;
 
-	public function initialize() {
-		$this->belongsTo('clientID', 'Client', 'clientID');
-	}
+        //check the client.
+        if (is_bool($this->getClient())) {
+            $valid = false;
+        }
 
-	/* 
-	 * This method is called before the model is saved to the database
-	 * i.e when model->save() is called. This validates that the 
-	 * requestID generated for this request is unique, and fails if not.
-	 */
-	public function validation() {
-		//perform validation on this model before saving/updating.
+        //check the timestamp.
+        if (!date('Y-m-d', $this->timestamp)) {
+            $valid = false;
+        }
 
-		//the requestID must be unique
-		$this->validate(new Uniqueness(
-			array(
-				'field' => 'requestID',
-				'message' => 'The requestID must be unique.'
-			)
-		));
+        //check the redirectURI
+        if (!filter_var($this->redirectURI, FILTER_VALIDATE_URL)) {
+            $valid = false;
+        }
 
-		//check if a validation message has been produced.
-		if($this->validationHasFailed()) {
-			return false;
-		}
-	}
+        //check the requestID.
+        if (!isset($this->requestID)) {
+            $valid = false;
+        }
 
-	/* 
-	 * This function validates that this request is valid.
-	 * i.e that all of the parameters in this request are valid.
-	 * 
-	 * @return bool TRUE if request is valid, FALSE otherwise.
-	 */
-	public function isValidRequest() {
+        return $valid;
+    }
 
-		//this request is treated as valid by default.
-		$valid = true;
+    public function getRequestID() {
+        return $this->requestID;
+    }
+    
+    /**
+     * returns the permissions array made with this request.
+     * @return array the permission array.
+     */
+    public function getScope() {
+        return $this->scope;
+    }
 
-		//check the client.
-		if(is_bool($this->getClient())) {
-			$valid = false;
-		}
+    /**
+     * Returns the client object associated with this  
+     * request or false if a client was not found.
+     * 
+     * @return \Models\Client - the client that made the request.
+     */
+    public function getClient() {
 
-		//check the timestamp.
-		if(!date('Y-m-d', $this->timestamp)) {
-			$valid = false;
-		}
+        //first check that the ID isset.
+        if (!isset($this->clientID)) {
+            return false;
+        }
 
-		//check the redirectURI
-		if(!filter_var($this->redirectURI, FILTER_VALIDATE_URL)) {
-			$valid = false;
-		}
+        //findFirst of the Model class finds the first entry
+        //matching the column(s) to value(s). binding is optional,
+        //but forcing placeholders and types on columns 
+        //reduces the risk of SQL injection.
+        $client = \Models\Client::findFirst(
+            array(
+                'conditions' => 'clientID = ?1',
+                'bind' => array(1 => $this->clientID),
+                'bindTypes' => array(1 => Column::BIND_TYPE_STR)
+            )
+        );
 
-		//check the requestID.
-		if(!isset($this->requestID)) {
-			$valid = false;
-		}
+        //just return the client as if one was not found,
+        //false will be returned anyway.
+        return $client;
+    }
 
-		return $valid;
+    /**
+     * Returns the redirect URI to use.
+     * Obviously we cannot return the one supplied
+     * by the client as a different one may have been
+     * provided.
+     *
+     * @return RedirectURI - the redirectURI. 
+     */
+    public function getRedirectURI() {
+        return $this->redirectURI;
+    }
 
-	}
+    /**
+     * Returns the UNIX timestamp of
+     * when this request was made.
+     *
+     * @return INT the UNIX timestamp.
+     */
+    public function getTimestamp() {
+        return $this->timestamp;
+    }
+    
+    /**
+     * set the permissions required in this request.
+     * @param array $scope the permissions array.
+     * @return $this returns itself for method 'chaining'
+     */
+    public function setScope($scope) {
+        $this->scope = $scope;
+        return $this;
+    }
 
-	/*********************/
-	/* Getters & Setters */
+    /**
+     * Sets the requestID for this request.
+     * This value is unique, but the validation
+     * for this is carried out in validation();
+     *
+     * @param $requestID the requestID for this request.
+     * @return $this returns itself for method 'chaining'
+     * @see /Models/Request->validation();
+     */
+    public function setRequestID($requestID) {
 
-	public function getRequestID() {
-		return $this->requestID;
-	}
+        //all validation on the ID are carried out
+        //in model validation.
+        $this->requestID = $requestID;
+        return $this;
+    }
 
-	/*
-	 * Returns the client object associated with this  
-	 * request or false if a client was not found.
-	 * 
-	 *@return Client - the client that made the request.
-	 */
-	public function getClient() {
+    /**
+     * Sets the RedirectURI to use for this request.
+     * @param $redirectURI the redirectURI to redirect to.
+     * @return $this returns itself for method 'chaining'
+     */
+    public function setRedirectURI($redirectURI) {
+        $this->redirectURI = $redirectURI;
+        return $this;
+    }
 
-		//first check that the ID isset.
-		if(!isset($clientID)) {
-			return false;
-		}
+    /**
+     * sets the clientID for this request.
+     * This needs to be called before we can call
+     * $this->getClient() as this searches the db for
+     * a /Models/Client entry that matches the ID.
+     *
+     * @param $clientID the id of the client this request is for.
+     * @return $this returns itself for method 'chaining'
+     */
+    public function setClientID($clientID) {
+        if (is_numeric($clientID)) {
+            $this->clientID = $clientID;
+        }
+        return $this;
+    }
 
-		//findFirst of the Model class finds the first entry
-		//matching the column(s) to value(s). binding is optional,
-		//but forcing placeholders and types on columns 
-		//reduces the risk of SQL injection.
-		$client = \Models\Client::findFirst(
-			array(
-				'conditions' => 'clientID = ?1',
-				'bind' => array(1 => $this->clientID),
-				'bindTypes' => array(1 => Column::BIND_TYPE_STR)
-			)
-		);
+    /**
+     * Sets the timestamp for when this request was made.
+     * 
+     * @param $timestamp the UNIX timestamp.
+     * @return $this returns itself for method 'chaining'
+     */
+    public function setTimestamp($timestamp) {
+        $this->$timestamp = $timestamp;
+        return $this;
+    }
 
-		//just return the client as if one was not found,
-		//false will be returned anyway.
-		return $client;
-	}
+    /**
+     * Generates a new Request object, this function also carrys out all of the required
+     * validation that is needed to be performed on a Request before it is deemed valid.
+     * @param string $clientID <p>The unique ID of the client making the request.</p>
+     * @param string|int $timestamp [optional] <p>The UNIX timestamp of when this request was made, 
+     * can either be a string function call i.e 'time()' or the int timestamp. Default is 'time()'</p>
+     * @param string $redirectURI [optional] <p>The redirectURI, default is null (use the clients default).</p>
+     * @return \Models\Request|boolean the request object on success, <b>FALSE</b> otherwise.
+     */
+    public static function requestBuilder($clientID, $scope, $timestamp = 'time', $redirectURI = null) {
+        //generate a requestID
+        $requestID = \Models\Request::generateID();
 
-	/*
-	 * Returns the redirect URI to use.
-	 * Obviously we cannot return the one supplied
-	 * by the client as a different one may have been
-	 * provided.
-	 *
-	 * @return RedirectURI - the redirectURI. 
-	 */
-	public function getRedirectURI() {
-		return $this->redirectURI;
-	}
+        //generate a new request.
+        $request = new \Models\Request();
+        $request->setRequestID($requestID)
+                ->setClientID($clientID)
+                ->setScope($scope);
+        
+        //check if the timestamp is a int or a function call.
+        if(!is_numeric($timestamp)) {
+            $timestamp = (function_exists($timestamp)) ? $timestamp() : time();
+        }
+        
+        $request->setTimestamp($timestamp);
 
-	/* 
-	 * Returns the UNIX timestamp of
-	 * when this request was made.
-	 *
-	 * @return INT the UNIX timestamp.
-	 */
-	public function getTimestamp() {
-		return $this->timestamp;
-	}
+        $client = $request->getClient();
 
-	/*
-	 * Sets the requestID for this request.
-	 * This value is unique, but the validation
-	 * for this is carried out in validation();
-	 *
-	 * @param $requestID the requestID for this request.
-	 * @return $this returns itself for method 'chaining'
-	 * @see /Models/Request->validation();
-	 */
-	public function setRequestID($requestID) {
+        if (is_bool($client)) {
+            return false; //could not find client.
+        }
 
-		//all validation on the ID are carried out
-		//in model validation.
-		$this->requestID = $requestID;
-		return $this;
-	}
+        //check if a redirect URI was provided and that it is valid.
+        if (isset($redirectURI)) {
+            if (filter_var($redirectURI, FILTER_VALIDATE_URL)) {
+                //check that the hostname is the same as the 
+                //one supplied for the client. (prevents XSS Attacks)
+                //parse the urls into associative array.
+                $url = parse_url($redirectURI);
+                $curl = parse_url($client->getDefaultRedirectURI());
 
-	/* 
-	 * Sets the RedirectURI to use for this request.
-	 * @param $redirectURI the redirectURI to redirect to.
-	 * @return $this returns itself for method 'chaining'
-	 */
-	public function setRedirectURI($redirectURI) {
-		$this->redirectURI = $redirectURI;
-		return $this;
-	}
+                //if the hosts are the same, regardless of case.
+                if (strtolower($curl['host']) == strtolower($url['host'])) {
+                    $request->setRedirectURI($redirectURI);
+                }
+            }
+        }
 
-	/*
-	 * sets the clientID for this request.
-	 * This needs to be called before we can call
-	 * $this->getClient() as this searches the db for
-	 * a /Models/Client entry that matches the ID.
-	 *
-	 * @param $clientID the id of the client this request is for.
-	 * @return $this returns itself for method 'chaining'
-	 */
-	public function setClientID($clientID) {
-		if(is_numeric($clientID)) {
-			$this->clientID = $clientID;
-		}
-		return $this;
-	}
+        //check if the redirect uri was set from the parameter, if not
+        //use the default.
+        $ru = $request->getRedirectURI();
+        if (!isset($ru)) {
+            $request->setRedirectURI($client->getDefaultRedirectURI());
+        }
 
-	/* 
-	 * Sets the timestamp for when this request was made.
-	 * 
-	 * @param $timestamp the UNIX timestamp.
-	 * @return $this returns itself for method 'chaining'
-	 */
-	public function setTimestamp($timestamp) {
-		$this->$timestamp = $timestamp;
-		return $this;
-	}
+        if (!$request->isValidRequest()) {
+            return false; //invalid request.
+        }
 
-	/* End Getters & Setters */
-	/*************************/
+        //attempt to save this request.
+        if (!$request->save()) {
 
-	/********************/
-	/* Static Functions */
+            //validation error, try again, generating a new ID.
+            \Models\Request::requestBuilder($clientID, $redirectURI, $timestamp);
+        }
 
-	/*
-	 * This function is a builder function to generate a request.
-	 * This function handles all of the validation and initialisation
-	 * of the request object and also saves it to the database.
-	 * 
-	 * @param $clientID 			  the ID of the client.
-	 * @param $redirectURI [optional] the redirectURI to redirect to
-	 * 								  after authorisation is complete,
-	 *								  default is the one provided by the client.
-	 * @param $timestamp   [optional] The UNIX timestamp of when this request was
-	 *								  made, default is now (as provided by time()).
-	 *
-	 * @return mixed This function either returns the newly generated request object, or false
-	 *				 if validation failed.
-	 */
-	public static function requestBuilder($clientID, $timestamp = time(), $redirectURI = null) {
-		//generate a requestID
-		$requestID = \Models\Request::generateID();
+        //return the request object.
+        return $request;
+    }
 
-		//generate a new request.
-		$request = new \Models\Request();
-		$request->setRequestID($requestID)
-				->setClientID($clientID)
-				->setTimestamp($timestamp);
+    /**
+     * Attempts to find a Request object by its requestID.
+     * This function also deletes the model from the database.
+     * 
+     * @return mixed the Request object on success, FALSE otherwise.
+     */
+    public static function findRequestByID($requestID) {
 
-		$client = $request->getClient();
+        $request = \Models\Request::findFirst(
+            array(
+                'conditions' => 'requestID = ?1',
+                'bind' => array(1 => $requestID),
+                'bindTypes' => array(1 => Column::BIND_TYPE_STR)
+            )
+        );
 
-		if(is_bool($client)) {
-			return false; //could not find client.
-		}
+        if (!is_bool($request)) {
+            $request->delete();
+        }
 
-		//check if a redirect URI was provided and that it is valid.
-		if(isset($redirectURI)) {
-			if(filter_var($redirectURI, FILTER_VALIDATE_URL)) {
-				//check that the hostname is the same as the 
-				//one supplied for the client. (prevents XSS Attacks)
-
-				//parse the urls into associative array.
-				$url = parse_url($redirectURI);
-				$curl = parse_url($client->getDefaultRedirectURI());
-
-				//if the hosts are the same, regardless of case.
-				if(strtolower($curl['host']) == strtolower($url['host'])) {
-					$request->setRedirectURI($redirectURI);
-				}
-			}
-		}
-
-		//check if the redirect uri was set from the parameter, if not
-		//use the default.
-		$ru = $request->getRedirectURI();
-		if(!isset($ru)) {
-			$request->setRedirectURI($client->getDefaultRedirectURI());
-		}
-
-		if(!$request->isValidRequest()) {
-			return false; //invalid request.
-		}
-
-		//attempt to save this request.
-		if(!$request->save()) {
-
-			//validation error, try again, generating a new ID.
-			\Models\Request::requestBuilder($clientID, $redirectURI, $timestamp);
-
-		}
-
-		//return the request object.
-		return $request;
-	}
-
-	/*
-	 * Attempts to find a Request object by its requestID.
-	 * This function also deletes the model from the database.
-	 * 
-	 * @return mixed the Request object on success, FALSE otherwise.
-	 */
-	public static function findRequestByID($requestID) {
-
-		$request = \Models\Request::findFirst(
-			array(
-				'conditions' => 'requestID = ?1',
-				'bind' => array(1 => $requestID),
-				'bindTypes' => array(1 => Column::BIND_TYPE_STR)
-			)
-		);
-
-		if(!is_bool($request)) {
-			$request->delete();
-		}
-
-		return $request;
-	}
-
-	/* End Static Functions */
-	/************************/
-
+        return $request;
+    }
 }
