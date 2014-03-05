11b4cfc8dc9193aaf10ac1dd7550aa3b09f6dd43
Jack Timblin <jack.timblin@partsit.com>
2014-03-05 16:45:46 +0000

    Completed a read-only version of a Token instance.

--- api/app/models/auth/Token.php
+++ api/app/models/auth/Token.php
@@ -6,11 +6,20 @@ use \Phalcon\Db\Column;
 
 /**
  * this class will encapsulate a Token saved on the Auth Server,
- * except it does not need functions to generate them.
+ * except it does not need functions to generate them, this class
+ * also has no set methods, as it is read only in this scope.
  * 
  * @Author - Jack Timblin - U1051575
  */
 class Token extends \Phalcon\Mvc\Model {
+    
+    private $scope;
+    
+    private $userID;
+    
+    public function getSource() {
+        return 'token';
+    }
     
     public function initialize() {
         $this->setConnectionService('authdb');
@@ -26,5 +35,12 @@ class Token extends \Phalcon\Mvc\Model {
         
     }
     
+    public function getScope() {
+        return $this->scope;
+    }
+    
+    public function getUserID() {
+        return $this->userID;
+    }
 }
 
