d65b389d73e9d1ad7ddb6b22f95a5c9bb6a82a44
Jack <jacktimblin@gmail.com>
2014-03-06 22:53:03 +0000

    Fixed typo.

--- auth-server/app/models/BaseModel.php
+++ auth-server/app/models/BaseModel.php
@@ -4,7 +4,7 @@ namespace Models;
 
 use \Phalcon\Mvc\Model;
 
-abstract class BaseModel extends Model {
+class BaseModel extends Model {
 
     /**
      * This function just generates a random string of a given length by
@@ -21,26 +21,22 @@ abstract class BaseModel extends Model {
     
     public static function validateInput($params, $keys) {
         
-        $validRequest = true;
         //check all of the required keys are in the params.
         if(count(array_intersect($keys, array_keys($params))) != count($keys)) {
-            $validRequest = false;
+            return false;
         }
 
         foreach($keys as $key) {
             if(!is_string($params[$key]) || strlen($params[$key]) == 0) {
-                $validRequest = false;
+                return false;
             }
         }
         
-        return $validRequest;
+        return true;
     }
     
     public static function array_equal($a1, $a2) {
         return !array_diff($a1, $a2) && !array_diff($a2, $a1);
     }
-    
-    public function getSource();
-    public function validation();
 
 }
